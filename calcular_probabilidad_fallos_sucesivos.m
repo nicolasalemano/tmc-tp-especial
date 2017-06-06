8a7effa07cebaebc6463da6e27d442b828c579d3
nicolasalemano <nico.alemano@gmail.com>
2017-06-06 19:27:36 -0300

    update readme

---
+++ calcular_probabilidad_fallos_sucesivos.m
@@ -0,0 +1,51 @@
+%c=parcialitos+0.7*parcial
+
+
+%FUNCION 
+function [probabilidad, todas_las_probabilidades]=calcular_probabilidad_fallos_sucesivos(epsilon,DNI)
+
+ todas_las_probabilidades = [];
+
+  probabilidad_anterior = 0; 
+  probabilidad_actual = 1;  
+  casos_favorables = 0;
+ contador=0;
+ valorDNI_ante=0
+ tic
+  while ~converge(probabilidad_anterior, probabilidad_actual, epsilon, contador)
+          
+        probabilidad_anterior = probabilidad_actual;
+        
+        valorDNI=my_mex_service(DNI);
+               
+        contador=contador+1;        
+
+        if((valorDNI_ante==0)&&(valorDNI==0))
+            casos_favorables = casos_favorables + 1;
+         end
+                  
+
+                    
+         probabilidad_actual = casos_favorables / contador;
+           
+                   % TODO: Programar esta funcion para que calcule la probabilidad de acertarle
+        % utilizando Montecarlo
+
+        % Descomentar las siguientes 2 lineas para imprimir por pantalla las probabilidades
+        %fprintf('%f\n', probabilidad_actual);
+      %  fflush(stdout);
+        
+        % Colocar esto adentro del bucle
+        % Esto es solamente para poder hacer el grafico ----------------------------
+        todas_las_probabilidades = cat(1, todas_las_probabilidades, probabilidad_actual);
+        % --------------------------------------------------------------------------
+         valorDNI_ante=valorDNI;
+  end
+  toc%muestra el tiempo que tarda en ejecutar
+    probabilidad = probabilidad_actual;
+
+fprintf('%d\n', contador);
+ 
+ 
+ 
+ end
--- script_trabajo_especial.m
+++ script_trabajo_especial.m
@@ -1,3 +1,49 @@
 %script trabajo especial
 clear
 clc
+
+%SCRIPT
+
+epsilon = 0.1;
+DNI=28872690;
+
+
+[probabilidad, todas_las_probabilidades]=calcular_probabilidad_fallos_sucesivos(epsilon,DNI);
+fprintf('Probabilidad actual: %f\n', probabilidad);
+fprintf('epsilon: %i\n', epsilon);
+fprintf('DNI: %i\n', DNI);
+figure, plot(todas_las_probabilidades);
+
+xlabel('Numero de iteracion');
+ylabel('Probabilidad');
+ylim([0 1]);
+legend('Evolucion de la probabilidad');
+grid on
+
+
+epsilon = 0.01;
+[probabilidad, todas_las_probabilidades]=calcular_probabilidad_fallos_sucesivos(epsilon,DNI);
+fprintf('Probabilidad actual 2°: %f\n', probabilidad);
+fprintf('epsilon 2°: %i\n', epsilon);
+fprintf('DNI 2°: %i\n', DNI);
+figure, plot(todas_las_probabilidades);
+
+xlabel('Numero de iteracion');
+ylabel('Probabilidad');
+ylim([0 1]);
+legend('Evolucion de la probabilidad');
+grid on
+
+epsilon = 0.00001;
+[probabilidad, todas_las_probabilidades]=calcular_probabilidad_fallos_sucesivos(epsilon,DNI);
+fprintf('Probabilidad actual 3°: %f\n', probabilidad);
+fprintf('epsilon 3°: %i\n', epsilon);
+fprintf('DNI 3°: %i\n', DNI);
+figure, plot(todas_las_probabilidades);
+
+xlabel('Numero de iteracion');
+ylabel('Probabilidad');
+ylim([0 1]);
+legend('Evolucion de la probabilidad');
+grid on
+
